6b93fa51c4680fb99105cbe6f85ba3e18a5914c1
shreegirikumar <shreegirikumar@gmail.com>
2026-04-05 15:31:03 -0400

    Project Skeleton

---
+++ src/test/java/com/example/java_mcp_demo/JavaMcpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_mcp_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMcpDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
